a4540d880be05ca9812822c4fc53f7b3a0561b10
Khedr <ahmedkhaderfcih@gmail.com>
2025-04-29 17:40:18 +0300

    Remove Google Auth Token

--- app/src/main/java/com/mobily/bugit/data/Config.kt
+++ app/src/main/java/com/mobily/bugit/data/Config.kt
@@ -3,5 +3,4 @@ package com.mobily.bugit.data
 object Config {
     const val spreadsheetId = "1ZPi11JUqiVYfbW3C3YDco0gtzvb58G6yre7WWwmY0OU"
     const val spreadsheetApiKey = "AIzaSyARvGa3bNTowPgZtYsWkO_1watagJCWQ_w"
-    const val spreadsheetTokenKey = "Bearer ya29.a0AZYkNZimBeVbrppBfwi-5_fH2A8nUnxmbqXDHAAwTIIPZMjRyoVwAjhxw-F-JO_0aAw4_jz-UeW7GkXIl4kL9EE1l1M-_9z5e1ZzKqb6ZFFwtXMItApYYMx1hzdfdqTyZrEUWbDf0G66wHYumK_AwYo1yCudy5sOZwLPdIFKaCgYKAV4SARUSFQHGX2MigDiKEcHirpuJEOa8ksJ7LA0175"
 }
